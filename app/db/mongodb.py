a5911548fa9bdcbba3c0b2f5d18d1517ff49d4a9
AlexLueng <alexlueng000@gmail.com>
2026-01-07 18:05:49 +0800

    update

--- app/db/mongodb.py
+++ app/db/mongodb.py
@@ -1,7 +1,16 @@
+import os
 from motor.motor_asyncio import AsyncIOMotorClient
 
-MONGO_DB_URL = "mongodb://localhost:27017"
-MONGO_DB_NAME = "bidding_info_new"
+MONGO_USER = os.getenv("MONGO_USER")
+MONGO_PASS = os.getenv("MONGO_PASS")
+MONGO_HOST = os.getenv("MONGO_HOST", "localhost")
+MONGO_PORT = os.getenv("MONGO_PORT", "27017")
+MONGO_DB_NAME = os.getenv("MONGO_DB_NAME", "bidding_info_new")
+
+if MONGO_USER and MONGO_PASS:
+    MONGO_DB_URL = f"mongodb://{MONGO_USER}:{MONGO_PASS}@{MONGO_HOST}:{MONGO_PORT}"
+else:
+    MONGO_DB_URL = f"mongodb://{MONGO_HOST}:{MONGO_PORT}"
 
 client = AsyncIOMotorClient(MONGO_DB_URL)
 # print(client)
